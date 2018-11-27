649a77c3204a9326d1b6df5b6245fa0c26b4aafc
Tavish <bit_zjm@hotmail.com>
2018-11-28 00:16:47 +0800

    update 60-63 solutions 18/11/28 0:16

---
+++ src/pers/tavish/solution/medium/RotateList.java
@@ -0,0 +1,91 @@
+package pers.tavish.solution.medium;
+
+import org.junit.Test;
+
+/*
+
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+
+Example 2:
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+
+*/
+
+public class RotateList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return next != null ? val + "->" + next.toString() : val + "->" + null;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode tmp = head;
+        int length = 1;
+        while (tmp.next != null) {
+            ++length;
+            tmp = tmp.next;
+        }
+
+        if (k % length == 0) {
+            return head;
+        }
+
+        if (k > length) {
+            k %= length;
+        }
+
+        ListNode tail = tmp;
+        tmp = head;
+        for (int i = 0; i < length - 1 - k; i++) {
+            tmp = tmp.next;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = tmp.next;
+        tmp.next = null;
+        tail.next = head;
+
+        return dummy.next;
+    }
+
+    @Test
+    public void testCase() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println(rotateRight(node1, 10));
+    }
+}
---
+++ src/pers/tavish/solution/medium/UniquePaths.java
@@ -0,0 +1,75 @@
+package pers.tavish.solution.medium;
+
+import org.junit.Test;
+
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Example 1:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+Input: m = 7, n = 3
+Output: 28
+
+for more information: https://leetcode.com/problems/unique-paths/description/
+*/
+public class UniquePaths {
+    // bottom-up
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][1] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= m; i++) {
+            for (int j = 2; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m][n];
+    }
+/*
+ * Top-down (TLE)
+    public int uniquePaths(int m, int n) {
+        return m == 1 || n == 1 ? 1 : uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+*/
+/*
+ *  Top-down 
+    public int uniquePaths(int m, int n) {
+        return uniquePaths(m, n, new int[m + 1][n + 1]);
+    }
+    
+    private int uniquePaths(int m, int n, int[][] map) {
+    
+        if (m == 1 || n == 1) {
+            return 1;
+        } else {
+            if (map[m][n] != 0) {
+                return map[m][n];
+            } else {
+                map[m][n] = uniquePaths(m - 1, n, map) + uniquePaths(m, n - 1, map);
+                return map[m][n];
+            }
+        }
+    }
+*/
+    
+    @Test
+    public void testCase() {
+        System.out.println(uniquePaths(51, 9));
+    }
+}
