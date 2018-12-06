91d7036d5af5dcc1536cc2b4f60b19c530b04028
Tavish <bit_zjm@hotmail.com>
2018-12-06 23:57:05 +0800

    update 91-92 solutions 18/12/6 23:57

---
+++ src/pers/tavish/solution/medium/ReverseLinkedListII.java
@@ -0,0 +1,81 @@
+package pers.tavish.solution.medium;
+
+import org.junit.Test;
+
+/*
+
+Reverse a linked list from position m to n. Do it in one-pass.
+
+Note: 1 ≤ m ≤ n ≤ length of list.
+
+Example:
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+
+for more information: https://leetcode.com/problems/reverse-linked-list-ii/description/
+*/
+
+public class ReverseLinkedListII {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return next != null ? val + "->" + next : val + "";
+        }
+    }
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+        if (m == n) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(Integer.MAX_VALUE);
+        dummy.next = head;
+
+        ListNode curr = dummy;
+        for (int i = 0; i < m - 1; i++) {
+            curr = curr.next;
+        }
+
+        ListNode start = curr.next;
+        curr.next = null;
+        ListNode p = start, q = null;
+        for (int i = 0; i <= n - m; i++) {
+            ListNode pr = p.next;
+            p.next = q;
+            if (i == n - m) {
+                curr.next = p;
+                start.next = pr;
+            } else {
+                q = p;
+                p = pr;
+            }
+        }
+        return dummy.next;
+    }
+
+    @Test
+    public void testCase() {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        ListNode list = createList(nums);
+        System.out.println(list);
+        System.out.println(reverseBetween(list, 2, 5));
+    }
+
+    private ListNode createList(int[] nums) {
+        ListNode dummy = new ListNode(Integer.MAX_VALUE);
+        ListNode head = dummy;
+        for (int i : nums) {
+            head.next = new ListNode(i);
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
