94d53a1b7d8b088b610b9c8e8ae86df6871005e6
Tavish <bit_zjm@hotmail.com>
2018-12-08 23:05:03 +0800

    update 93-96 solutions 18/12/8 23:05

---
+++ src/pers/tavish/solution/medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,73 @@
+package pers.tavish.solution.medium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * 
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+For more information: https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+*/
+
+// 中序遍历
+public class BinaryTreeInorderTraversal {
+
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        List<Integer> res = new LinkedList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        
+        TreeNode curr = root;
+        while (!stack.isEmpty() || curr != null) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            if (!stack.isEmpty()) {
+                curr = stack.pop();
+                res.add(curr.val);
+                curr = curr.right;
+            }
+        }
+        return res;
+    } 
+/*
+ * recursive, MLE
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderTraversal(root, res);
+        return res;
+    }
+
+    private void inorderTraversal(TreeNode node, List<Integer> res) {
+        while (node != null) {
+            inorderTraversal(node.left, res);
+            res.add(node.val);
+            inorderTraversal(node.right, res);
+        }
+    }
+*/
+}
