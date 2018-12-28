65d630b4ead6b5c6fe14ff811873f553fad2c200
Tavish <bit_zjm@hotmail.com>
2018-12-28 23:08:46 +0800

    update struct

---
+++ src/pers/tavish/struct/RandomListNode.java
@@ -0,0 +1,11 @@
+package pers.tavish.struct;
+
+public class RandomListNode {
+
+    public int label;
+    public RandomListNode next, random;
+
+    public RandomListNode(int x) {
+        label = x;
+    }
+}
