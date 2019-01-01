fcb32f7960857024a58de45a706b74f67e6dc815
Tavish <bit_zjm@hotmail.com>
2019-01-02 02:29:55 +0800

    add dp solution

--- src/pers/tavish/solution/easy/MaximumSubarray.java
+++ src/pers/tavish/solution/easy/MaximumSubarray.java
@@ -31,6 +31,21 @@ public class MaximumSubarray {
         return global;
     }
 
+/*
+ * dp
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int res = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] < 0 ? 0 : dp[i - 1]);
+            res = Math.max(dp[i], res);
+        }
+    }
+ */
     @Test
     public void testCase() {
         int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
