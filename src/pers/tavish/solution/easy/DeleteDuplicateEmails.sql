ed0444d3d31a8e5e86d2dd1810f868067142fdd6
Tavish <bit_zjm@hotmail.com>
2019-02-28 22:30:58 +0800

    update 194-199 solutions 22:30 19/2/28

---
+++ src/pers/tavish/solution/easy/DeleteDuplicateEmails.sql
@@ -0,0 +1,33 @@
+/*
+
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Note:
+
+Your output is the whole Person table after executing your sql. Use delete statement.
+
+for more information: https://leetcode.com/problems/delete-duplicate-emails/
+*/
+
+#MySQL
+delete p1 from Person p1 inner join Person p2 on p1.Email = p2.Email where p1.Id > p2.Id;
+
+# Use subquery
+delete from Person where Id not in
+(select Id from (select min(Id) from Person group by Email) sub);
