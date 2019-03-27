cf1cef93ee015e3a502b7866163c69eebea60ef7
Tavish <bit_zjm@hotmail.com>
2019-03-27 21:28:15 +0800

    update 237-241 solutions 21:27 19/3/27

---
+++ src/pers/tavish/solution/medium/DifferentWaysToAddParentheses.java
@@ -0,0 +1,85 @@
+package pers.tavish.solution.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/*
+
+Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
+
+Example 1:
+Input: "2-1-1"
+Output: [0, 2]
+Explanation: 
+((2-1)-1) = 0 
+(2-(1-1)) = 2
+
+Example 2:
+Input: "2*3-4*5"
+Output: [-34, -14, -10, -10, 10]
+Explanation: 
+(2*(3-(4*5))) = -34 
+((2*3)-(4*5)) = -14 
+((2*(3-4))*5) = -10 
+(2*((3-4)*5)) = -10 
+(((2*3)-4)*5) = 10
+
+for more information: https://leetcode.com/problems/different-ways-to-add-parentheses/description/
+*/
+
+public class DifferentWaysToAddParentheses {
+    public List<Integer> diffWaysToCompute(String input) {
+        if (input == null || input.isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<String> ops = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            int j = i;
+            while (j < input.length() && Character.isDigit(input.charAt(j))) {
+                ++j;
+            }
+            ops.add(input.substring(i, j));
+            if (j != input.length()) {
+                ops.add(input.substring(j, j + 1));
+            }
+            i = j;
+        }
+        return calculate(ops, 0, ops.size() - 1);
+    }
+
+    private List<Integer> calculate(List<String> ops, int lo, int hi) {
+        List<Integer> res = new ArrayList<>();
+        if (lo == hi) {
+            res.add(Integer.parseInt(ops.get(lo)));
+            return res;
+        } else {
+            for (int i = lo + 1; i < hi; i += 2) {
+                String op = ops.get(i);
+                List<Integer> left = calculate(ops, lo, i - 1), right = calculate(ops, i + 1, hi);
+                for (int leftNum : left) {
+                    for (int rightNum : right) {
+                        switch (op) {
+                        case "+":
+                            res.add(leftNum + rightNum);
+                            break;
+                        case "-":
+                            res.add(leftNum - rightNum);
+                            break;
+                        default:
+                            res.add(leftNum * rightNum);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    
+    @Test
+    public void testCase() {
+        System.out.println(diffWaysToCompute("11+2-30+4"));
+    }
+}
---
+++ src/pers/tavish/solution/medium/SearchA2DMatrixII.java
@@ -0,0 +1,77 @@
+package pers.tavish.solution.medium;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+    - Integers in each row are sorted in ascending from left to right.
+    - Integers in each column are sorted in ascending from top to bottom.
+
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+for more information: https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+*/
+
+public class SearchA2DMatrixII {
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] > target) {
+                --j;
+            } else if (matrix[i][j] < target) {
+                ++i;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // binarySearch for every row
+//    public boolean searchMatrix(int[][] matrix, int target) {
+//        if (matrix == null || matrix.length == 0) {
+//            return false;
+//        }
+//        for (int i = 0; i < matrix.length; i++) {
+//            if (Arrays.binarySearch(matrix[i], target) >= 0) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+    
+    @Test
+    public void testCase() {
+        int[][] matrix = {
+                {1,  4,  7,  11, 15},
+                {2,  5,  8,  12, 19},
+                {3,  6,  9,  16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        assertEquals(true, searchMatrix(matrix, 18));
+    }
+}
