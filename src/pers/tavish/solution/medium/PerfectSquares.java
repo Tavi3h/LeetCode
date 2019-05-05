a49e9e2919a13309a71af89b34c9751e67934f8d
Tavish <bit_zjm@hotmail.com>
2019-05-05 15:07:01 +0800

    update 279 solution 15:06 19/5/5

---
+++ src/pers/tavish/solution/medium/PerfectSquares.java
@@ -0,0 +1,31 @@
+package pers.tavish.solution.medium;
+
+/*
+
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+
+for more information: https://leetcode.com/problems/perfect-squares/description/
+*/
+
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
+    }
+}
